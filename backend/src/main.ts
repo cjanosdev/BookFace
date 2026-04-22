7562ab207d45300bd31604de081e227a024f877f
cjanosdev <CjanosDev@protonmail.com>
2026-04-22 17:16:31 -0400

    enabling cors and environment based port

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // enable cors and env based port
+  app.enableCors({
+    origin: process.env.FRONTEND_RUL || 'http://localhost:5173',
+    credentials: true,
+  })
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
